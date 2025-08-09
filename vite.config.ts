01a4cef85d05b89b74cc00bccc7a99d1b9bcfcd5
Vitaly <VvitaliiKachaevv356@yandex.ru>
2025-08-09 20:02:02 +0400

    diplom

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/fe_diploma",
+  base: "/fe_diplom/",
   server: {
     open: true,
   },
